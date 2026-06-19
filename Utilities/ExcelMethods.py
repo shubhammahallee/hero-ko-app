1588a71512a848612ef17a055bbf7c568a8de11c
Shubham Mahalle <shubhammahallee@gmail.com>
2026-06-19 09:35:49 +0530

    Create ExcelMethods.py

---
+++ Utilities/ExcelMethods.py
@@ -0,0 +1,22 @@
+import openpyxl
+
+
+class ExcelMethods_class:
+
+    def __init__(self, file):
+        # File ek hi baar khulegi — constructor me
+        self.file = file
+        self.workbook = openpyxl.load_workbook(file)
+
+    def getRowCount(self, sheetName):
+        sheet = self.workbook[sheetName]
+        return sheet.max_row
+
+    def read_data(self, sheetName, rownum, columnno):
+        sheet = self.workbook[sheetName]
+        return sheet.cell(row=rownum, column=columnno).value
+
+    def write_data(self, sheetName, rownum, columnno, data):
+        sheet = self.workbook[sheetName]
+        sheet.cell(row=rownum, column=columnno).value = data
+        self.workbook.save(self.file)

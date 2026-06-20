96784ede36cd7dc19739a3d9e29766e159c5f2a5
Shubham Mahalle <shubhammahallee@gmail.com>
2026-06-20 08:03:50 +0530

    Update test_page02.py

--- testCases/test_page02.py
+++ testCases/test_page02.py
@@ -1,1 +1,12 @@
+import pytest
+from Pages.Page02 import Page02
 
+@pytest.mark.skip
+@pytest.mark.usefixtures("setup")
+class Test_Pagetwo:
+
+    def test_verify(self):
+        p2 = Page02(self.driver)
+        p2.element_page()
+        p2.upload_and_download()
+        p2.download()

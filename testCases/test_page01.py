3b297f12ed7f28720e50954a5be5d92acbb7ceee
Shubham Mahalle <shubhammahallee@gmail.com>
2026-06-20 08:03:07 +0530

    Update test_page01.py

--- testCases/test_page01.py
+++ testCases/test_page01.py
@@ -1,1 +1,16 @@
+import pytest
+from Pages.Page01 import Page01
 
+@pytest.mark.skip
+@pytest.mark.usefixtures("setup")
+class Test_Pageone:
+
+    def test_verify_title(self):
+        p1 = Page01(self.driver)
+        p1.element_page()
+        p1.text_box_button()
+        p1.enter_full_name()
+        p1.enter_email()
+        p1.enter_address()
+        p1.enter_perm_add()
+        p1.click_button_submit()

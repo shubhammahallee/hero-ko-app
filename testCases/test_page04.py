7f1c3c9ccff7543c29fbc9dfcf72aed6b56bf73e
Shubham Mahalle <shubhammahallee@gmail.com>
2026-06-22 08:57:21 +0530

    Update test_page04.py

--- testCases/test_page04.py
+++ testCases/test_page04.py
@@ -1,1 +1,11 @@
+import pytest
+from Pages.Page04 import Page04
 
+
+@pytest.mark.usefixtures("setup")
+class Test_Pagefour:
+
+    def test_verity_all_test(self, setup):
+        p4 = Page04(setup)
+
+        p4.click_buttons()

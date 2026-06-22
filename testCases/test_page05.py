8df27b8d953741c6f572da8376f3ef19bef53c20
Shubham Mahalle <shubhammahallee@gmail.com>
2026-06-22 09:01:38 +0530

    Update test_page05.py

--- testCases/test_page05.py
+++ testCases/test_page05.py
@@ -1,1 +1,18 @@
+import pytest
+from Pages.Page05 import Page05
+
+
+@pytest.mark.usefixtures("setup")
+class Test_Pagefive:
+
+    def test_verity_all_test(self, setup):
+        p5 = Page05(setup)
+
+        p5.click_widget_btn()
+        p5.click_date_picker()
+        p5.select_picker()
+        p5.click_select_month()
+        p5.click_select_year()
+        p5.click_select_date()
+        p5.click_select_time()
 

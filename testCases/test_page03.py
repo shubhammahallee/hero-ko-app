294419b63b0530b580a07b4b44da142aba65371b
Shubham Mahalle <shubhammahallee@gmail.com>
2026-06-22 08:56:59 +0530

    Update test_page03.py

--- testCases/test_page03.py
+++ testCases/test_page03.py
@@ -1,1 +1,16 @@
+import pytest
+from Pages.Page03 import Page03
+
+@pytest.mark.skip
+@pytest.mark.usefixtures("setup")
+class Test_Pagethree:
+
+    def test_verity_all_test(self, setup):
+        p3 = Page03(setup)
+
+        p3.click_element()
+        p3.click_checkbox()
+        p3.click_home()
+        p3.click_homelist()
+        p3.click_desktop()
 
